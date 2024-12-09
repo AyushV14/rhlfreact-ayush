b73f865c6d0945552a71bbba52662e5faea3af68
Ayush Vikharankar <vikharankarayush14@gmail.com>
2024-12-10 01:11:33 +0530

    Error fix for production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,11 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+// tailwind.config.js
+export default {
   content: [
+    "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-      colors:{
-        custompink:'#E64994'
-      }
-    },
+    extend: {},
   },
   plugins: [],
-}
+};
